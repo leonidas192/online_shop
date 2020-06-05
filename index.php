38d7e35844560ed4fe313ebf49c2581fdf7ecf51
muhammadaris2405 <muhammadaris2405@gmail.com>
2020-06-05 17:05:27 +0700

    sembarang

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  *
  * This content is released under the MIT License (MIT)
  *
- * Copyright (cccd) 2014 - 2019, British Columbia Institute of Technology
+ * Copyright (asdfgd) 2014 - 2019, British Columbia Institute of Technology
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
